737f804aa033b5080c74ec29c38145abeeec6942
DebHatim <thos.debboun.hatim@gmail.com>
2026-06-15 14:11:18 +0200

    chore: configure Kafka serialization and enable scheduling

--- src/main/java/com/hatim/alertas/AlertasApplication.java
+++ src/main/java/com/hatim/alertas/AlertasApplication.java
@@ -2,8 +2,10 @@ package com.hatim.alertas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // Para que el simulador funcione correctamente
 public class AlertasApplication {
 
 	public static void main(String[] args) {
